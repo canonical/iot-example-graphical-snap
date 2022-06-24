836a7ea8b8259174696fddfacb9ce755b4883536
Alan Griffiths <alan@octopull.co.uk>
2022-06-24 10:17:39 +0100

    Support parallel install

--- wayland-launch/bin/setup.sh
+++ wayland-launch/bin/setup.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 set -e
 
+SNAP_INSTANCE_NAME="$(readlink -f $0 | cut -f 3 -d /)"
+
 snap_connect_harder() {
   # Note the available slot providers
-  if ! snap connections %SNAP% | grep --quiet "^content.*%SNAP%:$1.*$"; then
+  if ! snap connections ${SNAP_INSTANCE_NAME} | grep --quiet "^content.*${SNAP_INSTANCE_NAME}:$1.*$"; then
     available_providers="$(snap interface "$1" | sed -e '1,/slots:/d')"
   else
     available_providers="$(snap interface content | sed -e '1,/slots:/d' | grep "$1")"
@@ -13,7 +15,7 @@ snap_connect_harder() {
   if [ "wayland" = "$1" ]; then
     for PROVIDER in ubuntu-frame mir-kiosk; do
        if echo "$available_providers" | grep --quiet "\- ${PROVIDER}"; then
-         sudo snap connect "%SNAP%:$1" "${PROVIDER}:$1"
+         sudo snap connect "${SNAP_INSTANCE_NAME}:$1" "${PROVIDER}:$1"
          return 0
        fi
     done
@@ -23,7 +25,7 @@ snap_connect_harder() {
 }
 
 for PLUG in %PLUGS%; do
-  if ! snap connections %SNAP% | grep --quiet "^.*%SNAP%:${PLUG}.*${PLUG}.*$"; then
-    sudo snap connect "%SNAP%:${PLUG}" 2> /dev/null || snap_connect_harder ${PLUG}
+  if ! snap connections ${SNAP_INSTANCE_NAME} | grep --quiet "^.*${SNAP_INSTANCE_NAME}:${PLUG}.*${PLUG}.*$"; then
+    sudo snap connect "${SNAP_INSTANCE_NAME}:${PLUG}" 2> /dev/null || snap_connect_harder ${PLUG}
   fi
 done
