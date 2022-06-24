ae80df118e159e47e49cf11997445240ba314f5b
Alan Griffiths <alan@octopull.co.uk>
2022-06-24 14:54:26 +0100

    Electron example

---
+++ wrapper/wrapper.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+export SNAP_URL="$(snapctl get url)"
+
+exec $SNAP/electron-helloworld/electron-quick-start \
+	--enable-features=UseOzonePlatform \
+	--ozone-platform=wayland \
+	--disable-dev-shm-usage \
+	--no-sandbox
