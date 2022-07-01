5ad74fe9190f1f871524f386ced6dbda050a4be3
Alan Griffiths <alan@octopull.co.uk>
2022-07-01 12:36:29 +0100

    Add additional recommended options

--- wrapper/wrapper.sh
+++ wrapper/wrapper.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 
 exec $SNAP/electron-helloworld/electron-quick-start \
-	--enable-features=UseOzonePlatform \
+	--enable-features=UseOzonePlatform,WaylandWindowDecorations \
 	--ozone-platform=wayland \
 	--disable-dev-shm-usage \
+	--enable-wayland-ime \
 	--no-sandbox
