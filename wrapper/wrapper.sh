919b4b64a68bc696a2d72303483655f08e2241a3
Alan Griffiths <alan@octopull.co.uk>
2022-07-04 17:09:32 +0100

    Drop WaylandWindowDecorations: it has been reported to cause problems and should be irrelevant to fullscreen operation

--- wrapper/wrapper.sh
+++ wrapper/wrapper.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 exec $SNAP/electron-helloworld/electron-quick-start \
-	--enable-features=UseOzonePlatform,WaylandWindowDecorations \
+	--enable-features=UseOzonePlatform \
 	--ozone-platform=wayland \
 	--disable-dev-shm-usage \
 	--enable-wayland-ime \
